fbb35b99f973ee762306b5cf2117f6d5fd9940d9
Yosf96633 <yosf96633@gmail.com>
2025-04-17 17:09:37 +0500

    Favorite section fixes

--- src/app/favorite/page.tsx
+++ src/app/favorite/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 import { Favorite } from "./Carousel";
 import React, { useEffect, useState } from "react";
-
+import { Loader } from "lucide-react";
+import Link from "next/link";
 interface Data {
   title: string;
   mediaType: "movie" | "tv";
@@ -11,24 +12,33 @@ interface Data {
 
 const page = () => {
   const [data, setData] = useState<Data[]>();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   useEffect(() => {
     const fetchData = async () => {
       const resposne = await fetch(`/api/favorite`);
       const result = await resposne.json();
-      const x = result?.data;
+      const x = result?.data || [];
       setData(x);
+      setIsLoading(false);
     };
     fetchData();
   }, []);
+  console.log(data);
   return (
     <div className=" min-h-screen flex flex-col items-center py-14 px-4">
-      {data?.length === 0 ? (
-        <p>No favorite found!</p>
+      {isLoading ? (
+        <div className=" h-full pt-36">
+          {" "}
+          <Loader className=" animate-spin" />
+        </div>
+      ) : data?.length === 0 ? (
+        <div className=" h-full pt-36 space-y-3">
+          <p className="text-center font-bold text-4xl max-md:text-3xl">No favorite found!</p>
+           <p className=" text-xl">Visit <Link className=" text-blue-400" href={'/'}>Home page</Link> and add your favorite movies</p>
+        </div>
       ) : (
         <>
-          <h1 className="text-center font-bold text-4xl">
-            Your Favorites
-          </h1>
+          <h1 className="text-center font-bold text-4xl max-md:text-3xl">Your Favorites</h1>
           <Favorite items={data ?? []} />
         </>
       )}
